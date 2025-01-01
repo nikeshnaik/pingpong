054100de3a2ba2c8c1e8dd2776c9b3ab51db7ab5
nikeshnaik <nikeshnaik.dev@gmail.com>
2025-01-01 11:04:24 +0530

    feat: ball fade

--- script/ball.gd
+++ script/ball.gd
@@ -1,11 +1,17 @@
 extends RigidBody2D
 
+@onready var trail = $Sprite2D/CPUParticles2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if linear_velocity.length() > 1:
+		trail.emitting = true
+		var angle = linear_velocity.angle()
+		trail.rotation = angle
+	else:
+		trail.emitting = false
